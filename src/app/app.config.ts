75d14ab6f3a63e42423ab17e901b656a32a22ec6
perolanegra <pedratto3@gmail.com>
2025-01-05 21:28:48 -0300

    funcionality work

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(AppRoutes),
     provideHttpClient(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(), provideAnimationsAsync()
   ],
 };
--- src/app/services/competition.service.ts
+++ src/app/services/competition.service.ts
@@ -2,11 +2,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../environment';
 
 @Injectable()
 export class CompetitionService {
-  private apiUrl = environment.apiUrl;
+  private apiUrl = 'http://localhost:3000/api';
 
   constructor(private http: HttpClient) {}
 
